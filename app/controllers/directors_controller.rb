6290ad7157b01af7e1777efc7cbb904ccf513dfe
ramirosc <ramiro@chicagobooth.edu>
2015-10-30 14:31:13 -0500

    HW directors done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,48 +1,54 @@
 class DirectorsController < ApplicationController
 
- def index
-    @directors = Director.all
-  end
+def index
+  @directors = Director.all
+end
+
+def new_form
+
+end
+
+
 
 def destroy
-    @directors = Director.find(params[:id])
+  @directors = Director.find(params[:id])
 
-    @directors.destroy
-  end
+  @directors.destroy
+end
 
 
-  def create_row
-    @directors = Director.new
-    @directors.dob = params[:dob]
-    @directors.name = params[:name]
-    @directors.bio = params[:bio]
-    @directors.image_url = params[:image_url]
+def create_row
+  @directors = Director.new
+  @directors.dob = params[:dob]
+  @directors.name = params[:name]
+  @directors.bio = params[:bio]
+  @directors.image_url = params[:image_url]
 
-    @directors.save
+  @directors.save
 
-    render("show_details")
-  end
+  redirect_to("http://www.google.com")
+end
 
 def show_details
-    @directors = Director.find(params[:id])
-  end
+  @directors = Director.find(params[:id])
+end
 
 def edit_form
-    @directors = Director.find(params[:id])
-  end
+  @directors = Director.find(params[:id])
+end
 
-  def update_row
-    @directors = Director.find(params[:id])
+def update_row
+  @directors = Director.find(params[:id])
 
-    @directors.dob = params[:dob]
-    @directors.name = params[:name]
-    @directors.bio = params[:bio]
-    @directors.image_url = params[:image_url]
+  @directors.dob = params[:dob]
+  @directors.name = params[:name]
+  @directors.bio = params[:bio]
+  @directors.image_url = params[:image_url]
 
-    @directors.save
+  @directors.save
 
-   render("show_details")
-  end
+  render("show_details")
+end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
 
+
+  # CREATE
+  get('/create_row', { :controller => 'directors', :action => 'create_row' })
+ get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
+
+
+
 #READ DIRECTORS
 get('/directors', { :controller => 'directors', :action => 'index' })
 get('/directors/:id', { :controller => 'directors', :action => 'show_details' })
 
 #DELETE
 get("/delete_directors/:id", { :controller => 'directors', :action => "destroy" })
-
-  # CREATE
-  get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
-  get('/create_row', { :controller => 'directors', :action => 'create_row' })
-
 
 #EDIT
  get('/directors/:id/edit', { :controller => 'directors', :action => 'edit_form' })
